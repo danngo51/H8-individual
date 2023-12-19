de94ee6077bd2d1f65baedc106f7de76140e903a
Dan Ngo <danngo51@gmail.com>
2023-12-19 15:29:02 +0700

    model and migration

--- app/Models/Subpage.php
+++ app/Models/Subpage.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Subpage extends Model
 {
     use HasFactory;
+
+     // The table associated with the model.
+     protected $table = 'posts';
+
+     // The attributes that are mass assignable.
+     protected $fillable = [
+         'title',
+         'description',
+         'user_id', // Make sure this is the name of the foreign key column for the user.
+     ];
+ 
+     /**
+      * The user who authored the post.
+      */
+     public function user()
+     {
+         return $this->belongsTo(User::class);
+     }
+     
+     public function comments()
+     {
+         return $this->hasMany(Comment::class);
+     }
+
 }
--- database/migrations/2023_12_19_081243_create_subpages_table.php
+++ database/migrations/2023_12_19_081243_create_subpages_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('subpages', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('description');
             $table->timestamps();
         });
     }
