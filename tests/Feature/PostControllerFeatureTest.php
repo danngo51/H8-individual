65dda8d5c04dd5e4e14a93ceefe23d605c9a36bb
Dan Ngo <danngo51@gmail.com>
2023-12-28 03:32:07 +0700

    psots

--- tests/Feature/PostControllerFeatureTest.php
+++ tests/Feature/PostControllerFeatureTest.php
@@ -2,19 +2,62 @@
 
 namespace Tests\Feature;
 
+use Tests\TestCase; // Extends 'use PHPUnit\Framework\TestCase;' that is why it is not imported here
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use App\Models\Subpage;
+use App\Models\User;
 
 class PostControllerFeatureTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
+    use RefreshDatabase;
+
+    public function it_displays_create_post_form()
+    {
+        $subpage = Subpage::factory()->create();
+        $response = $this->get(route('posts.create', $subpage->id));
         $response->assertStatus(200);
+        $response->assertViewIs('posts.create');
+        $response->assertViewHas('subpage', $subpage);
     }
+
+    public function it_stores_a_post_and_redirects()
+    {
+        $user = User::factory()->create();
+        $subpage = Subpage::factory()->create();
+        $this->actingAs($user);
+
+        $postData = ['title' => 'Test Post', 'content' => 'Test content'];
+        $response = $this->post(route('posts.store', $subpage->slug), $postData);
+
+        $response->assertRedirect(route('subpages.showSubpage', $subpage->slug));
+        $this->assertDatabaseHas('posts', ['title' => 'Test Post']);
+    }
+
+    public function it_toggles_like_on_a_post()
+    {
+        $user = User::factory()->create();
+        $subpage = Subpage::factory()->create();
+        $post = Post::factory()->create(['subpage_id' => $subpage->id]);
+        $this->actingAs($user);
+
+        $response = $this->post(route('posts.toggleLike', ['slug' => $subpage->slug, 'post_slug' => $post->slug]));
+        $response->assertRedirect();
+        $this->assertDatabaseHas('likes', ['user_id' => $user->id, 'post_id' => $post->id]);
+    }
+
+    public function it_deletes_a_post()
+    {
+        $user = User::factory()->create();
+        $subpage = Subpage::factory()->create();
+        $post = Post::factory()->create(['user_id' => $user->id, 'subpage_id' => $subpage->id]);
+        $this->actingAs($user);
+
+        $response = $this->delete(route('posts.destroy', ['slug' => $subpage->slug, 'postSlug' => $post->slug]));
+        $response->assertRedirect();
+        $this->assertDatabaseMissing('posts', ['id' => $post->id]);
+    }
+
+
 }
--- tests/Unit/PostControllerTest.php
+++ tests/Unit/PostControllerTest.php
@@ -6,11 +6,16 @@ use PHPUnit\Framework\TestCase;
 
 class PostControllerTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
+    public function it_generates_a_unique_slug_for_new_post()
     {
-        $this->assertTrue(true);
+        $subpage = Subpage::factory()->create();
+        $postTitle = 'New Post Title';
+        $expectedSlug = 'new-post-title';
+    
+        Post::factory()->create(['title' => $postTitle, 'subpage_id' => $subpage->id, 'slug' => $expectedSlug]);
+        
+        $newSlug = $this->callPrivateMethod($this->postController, 'generateSlug', [$postTitle, $subpage->id]);
+        
+        $this->assertEquals('new-post-title-1', $newSlug);
     }
 }
