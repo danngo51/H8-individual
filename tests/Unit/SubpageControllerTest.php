57da8f82b010d417ccce860dec05a26a3d8e6c86
Dan Ngo <danngo51@gmail.com>
2023-12-28 03:09:45 +0700

    testin

--- tests/Unit/SubpageControllerTest.php
+++ tests/Unit/SubpageControllerTest.php
@@ -3,42 +3,65 @@
 namespace Tests\Unit;
 
 use Tests\TestCase; // Extends 'use PHPUnit\Framework\TestCase;' that is why it is not imported here
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\Models\User;
+use App\Http\Controllers\SubpageController;
 use App\Models\Subpage;
-use Illuminate\Support\Str;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class SubpageControllerTest extends TestCase
 {
-    use RefreshDatabase;
-
-    public function testStoreMethod()
+    public function testShowSubpage()
     {
-        // Create a user for authentication
-        $user = User::factory()->create();
-
-        // Acting as the authenticated user
-        $this->actingAs($user);
-
-        // Define subpage data
-        $subpageData = [
+        // Mock a Subpage instance
+        $subpage = new Subpage([
             'name' => 'Test Subpage',
-            'description' => 'This is a test subpage.',
-        ];
-
-        // Post request to store method
-        $response = $this->post('/subpages', $subpageData);
-
-        // Assert that the subpage was created successfully
-        $response->assertStatus(302); // Redirect response
-        $this->assertDatabaseHas('subpages', [
-            'name' => 'Test Subpage',
-            'description' => 'This is a test subpage.',
-            'owner_id' => $user->id,
-            'slug' => Str::slug('Test Subpage'),
+            'description' => 'Test description',
+            'slug' => 'test-subpage',
         ]);
 
-        // Assert that the user is subscribed to the subpage
-        $this->assertTrue($user->subscriptions()->where('name', 'Test Subpage')->exists());
+        // Mock a request with the slug
+        $request = new Request(['slug' => 'test-subpage']);
+
+        // Create an instance of SubpageController
+        $controller = new SubpageController();
+
+        // Mock the behavior of the Eloquent model's findOrFail method
+        $this->expectException(ModelNotFoundException::class);
+
+        // Call the showSubpage method with the mock subpage and request
+        $view = $controller->showSubpage($subpage->slug);
+    }
+
+    public function testShowAll()
+    {
+        // Create an instance of SubpageController
+        $controller = new SubpageController();
+
+        // Call the showAll method
+        $view = $controller->showAll();
+
+        // Assert that the result is a View instance
+        $this->assertInstanceOf(View::class, $view);
+
+        // You can add more specific assertions based on your controller's behavior
+    }
+
+    public function testSearch()
+    {
+        // Create an instance of SubpageController
+        $controller = new SubpageController();
+
+        // Create a mock request with a search term
+        $request = new Request(['search' => 'Test Subpage']);
+
+        // Call the search method with the mock request
+        $view = $controller->search($request);
+
+        // Assert that the result is a View instance
+        $this->assertInstanceOf(View::class, $view);
+
+        // You can add more specific assertions based on your controller's behavior
     }
 }
--- tests/Unit/SubscribeControllerTest.php
+++ tests/Unit/SubscribeControllerTest.php
@@ -4,7 +4,7 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class SubscriptionControllerTest extends TestCase
+class SubscribeControllerTest extends TestCase
 {
     /**
      * A basic unit test example.
