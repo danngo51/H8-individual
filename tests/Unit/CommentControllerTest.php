95f2baff937d8b52ca0352dd8c10e3e8a5b39481
Dan Ngo <danngo51@gmail.com>
2023-12-28 12:05:14 +0700

    das

--- tests/Unit/CommentControllerTest.php
+++ tests/Unit/CommentControllerTest.php
@@ -2,12 +2,33 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Http\Controllers\CommentController;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Mockery;
+
 
 class CommentControllerTest extends TestCase
 {
-    /**
-     * No unit tests
-     */
-   
+    public function it_toggles_a_like_on_a_comment()
+    {
+        $user = User::factory()->make();
+        $comment = Mockery::mock(Comment::class);
+        $comment->shouldReceive('isLikedByUser')->once()->with($user)->andReturn(false);
+        $comment->shouldReceive('likes')->once()->andReturnSelf();
+        $comment->shouldReceive('attach')->once()->with($user->id);
+
+        // Mock the Auth facade to return the user's ID
+        Auth::shouldReceive('id')->once()->andReturn($user->id);
+        Auth::shouldReceive('user')->once()->andReturn($user);
+
+        // Call the method
+        $response = (new CommentController())->toggleLike($comment);
+
+        // Assertions
+        $this->assertNotNull($response);
+    
+    }
 }
--- tests/Unit/SubscriptionControllerTest.php
+++ tests/Unit/SubscriptionControllerTest.php
@@ -2,12 +2,17 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Http\Controllers\SubscriptionController;
+use App\Models\Subpage;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Mockery;
 
 class SubscriptionControllerTest extends TestCase
 {
-    /**
-     * no subscription unit tests
+    /*
+     * no tests
      */
     
 }
